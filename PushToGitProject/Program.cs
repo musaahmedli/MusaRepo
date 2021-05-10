95be2ad2d4701cf7eae4fcf80d2b3478d8dc3a5b
Musa Əhmədov <musa.ahmadov.t@asoiu.edu.az>
2021-05-10 13:33:05 +0400

    first version

---
+++ PushToGitProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushToGitProject
+{
+    class Program
+    {
+        public void GetUserMethod()
+        {
+
+        }
+        public string GetName()
+        {
+            return "name";
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
